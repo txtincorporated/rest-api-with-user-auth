d7c75630b1ccbca449b53e34a38184182829932a
Nathan P. Keene <npk@txtincorporated.com>
2016-11-08 00:12:26 -0800

    Add authorization and user management models, routes and other modules

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const app = require('./lib/app');
+const port = process.env.PORT || 3000;
 require('./lib/setup-mongoose');
 
-http.createServer(app).listen(3000);
+http.createServer(app).listen(port);  
