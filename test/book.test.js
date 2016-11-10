98601a83e0c096311c18ccf26a72a535170e794f
Nathan P. Keene <npk@txtincorporated.com>
2016-11-09 17:23:19 -0800

    All e2e and unit testing successful

---
+++ test/book.test.js
@@ -0,0 +1,35 @@
+const chai = require('chai'); 
+const assert = chai.assert;
+
+const Book = require('../lib/models/book'); 
+
+describe('Book model tests', () => {
+  it('fails without author, title, authId', done => {    
+    const book = new Book({
+      genres: ['novel']
+    });
+
+    book.validate(err => {
+      assert.isOk(err, 'author is required');
+      assert.isOk(err, 'title is required');
+      assert.isOk(err, 'authId is required');
+      done();
+    });
+  });
+
+  it('supplies default value for genres', done => {
+    const book = new Book({
+      title: 'Genji Monogatari',
+      author: 'Murasaki Shikibu',
+      authId: ''
+    });
+
+    book.validate(() => {
+      console.log('Genres: ', book.genres);
+      assert.isOk(book.genres);
+      done();
+    });
+
+  });
+
+});
--- test/books.api.test.js
+++ test/books.api.test.js
@@ -19,7 +19,7 @@ describe('test books resource route', () => {
   before(done => {
     const drop = () => connection.db.dropDatabase(done);
     if(connection.readyState === 1) drop();
-    else connection.on('open' ,drop);
+    else connection.on('open', drop);
   });
 
   // before(done => {  
